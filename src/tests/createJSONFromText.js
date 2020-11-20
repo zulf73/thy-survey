51362a0309f9eb7fdb73be3f54bc5578f3c2f586
Zulf Ahmed <zulf@pop-os.localdomain>
2020-11-19 18:29:47 -0600

    JSON working fine.  getting Mongo upload errors

--- src/tests/createJSONFromText.js
+++ src/tests/createJSONFromText.js
@@ -1,19 +1,13 @@
-const lineReader = require('line-reader');
+var fs = require('fs');
 
 function json_from_survey_text_file(file_path, qn){
-    var out = "\{ name: " + qn + ",";
-    out = out + " [ ";
-    var Promise = require('bluebird');
-    var eachLine = Promise.promisify(lineReader.eachLine);
-
-
-    eachLine(file_path, (line, last) => {
-	//console.log(line);
+    var data = fs.readFileSync( file_path, 'utf8' );
+    var lines = data.split("\n");
+    
+    var out = "\{ name: " + qn + ", value:[ ";
+    lines.forEach( (line) => {
 	if (line) {
 	    var v = line.split(':')
-	    //console.log(v[0])
-	    //console.log(v[1])
-
 	    var inc_out  = " \{ question:" + v[1] + ",";
 	    inc_out = inc_out + " answers: [";
 	    inc_out = inc_out + "  \{ type: \"1\", content: \"1\" \},";
@@ -23,15 +17,30 @@ function json_from_survey_text_file(file_path, qn){
 	    inc_out = inc_out + "  \{ type: \"5\", content: \"5\" \} ] \},";
 	    out = out + inc_out;
 	}
-    }).then( ()=>{
-	out = out + " ] \}";
-	//return JSON.parse(out);
-	console.log(out);
     });
-    
-    return(out)
+    out = out + " ] \}";
+    return(out);
 }
 
-// test code
-var testOut = json_from_survey_text_file( 'Proust.txt', 'Proust' );
-console.log(testOut);
+var testOut = json_from_survey_text_file( 'testQ.txt', 'testQ');
+fs.writeFileSync( 'testQ.json', testOut, 'utf8' );
+
+// let us upload it directly in 'survey'
+// read the JSON
+// insert the JSON in 'survey' collection
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://zulf:snaggle41014@cluster0.3cmqe.mongodb.net/<dbname>?retryWrites=true&w=majority";
+var fs = require('fs');
+var JSON = require('dirty-json');
+// these things always have promisify problems
+var jsonFileData = fs.readFileSync('./testQ.json', 'utf8');
+var jsonObject = JSON.parse(jsonFileData);
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("prod");
+  dbo.collection("surveys").insertOne(jsonObject, function(err, res) {
+    if (err) throw err;
+    db.close();
+  });
+});
